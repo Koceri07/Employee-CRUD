94d0e47b9f4fa611be2511cd2958ca214196db3f
Koceri07 <kocerimustafayev07@gmail.com>
2025-03-28 16:41:21 +0400

    Initial commit

---
+++ src/main/java/org/example/mentorjpa/MentorJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.mentorjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mentorjpa/MentorJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mentorjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
