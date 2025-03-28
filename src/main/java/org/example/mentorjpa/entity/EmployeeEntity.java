b386795791b4153605cc568c6e158389e63dd9a0
Koceri07 <kocerimustafayev07@gmail.com>
2025-03-28 16:58:36 +0400

    EmployeeEntity Class

---
+++ src/main/java/org/example/mentorjpa/entity/EmployeeEntity.java
@@ -0,0 +1,51 @@
+package org.example.mentorjpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Table(name = "employee")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String fin;
+    @Column
+    private int age;
+    @Column
+    private String brithdate;
+    @Column(name = "phone_number")
+    private int phoneNumber;
+    @Column
+    private String email;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeEntity that = (EmployeeEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
