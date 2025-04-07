49bef005f62212a5e063606088d498951a06e4fe
Koceri07 <kocerimustafayev07@gmail.com>
2025-04-07 19:11:45 +0400

    EmployeeDto updates

--- src/main/java/org/example/mentorjpa/dto/EmployeeDto.java
+++ src/main/java/org/example/mentorjpa/dto/EmployeeDto.java
@@ -1,19 +1,24 @@
 package org.example.mentorjpa.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDto {
     private Long id;
+    @Length(min = 3, max = 26)
     private String name;
+    @Length(min = 3, max = 26)
     private String surname;
     private String email;
+    @NotNull
     private String fin;
     private String brithdate;
     private int age;
-    private int phineNumber;
+    private int phoneNumber;
 }
